8ea9189c1080fe52aa5673861a24f3f465d2660f
Jane <tioj@uchicago.edu>
2023-04-20 12:41:22 -0500

    Initial Commit

---
+++ GestureApp/GestureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureAppApp.swift
+//  GestureApp
+//
+//  Created by Jane Tio on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GestureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
