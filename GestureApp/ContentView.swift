3588bc44a52f1fb08372bb0533dbd2f5f3cffae2
Jane <tioj@uchicago.edu>
2023-04-21 01:34:31 -0500

    sdf

--- GestureApp/ContentView.swift
+++ GestureApp/ContentView.swift
@@ -37,39 +37,45 @@ struct Tutorial: View {
     var tutorialShown: Bool = false
     
     @State private var pageIndex = 0
+    @State var finishedTutorial = false
     private let pages: [Page] = Page.samplePages
     private let dotAppearance = UIPageControl.appearance()
     
     var body: some View {
-        TabView(selection: $pageIndex) {
-            ForEach(pages) { page in
-                VStack {
-                    Spacer()
-                    PageView(page: page)
-                    Spacer()
-                    if page == pages.last {
-                        Button("Let's go!", action: goToZero)
-                            .buttonStyle(.bordered)
-                    } else {
-                        Button("next", action:
-                            incrementPage)
+        if finishedTutorial {
+            HomeScreen()
+        } else {
+            TabView(selection: $pageIndex) {
+                ForEach(pages) { page in
+                    VStack {
+                        Spacer()
+                        PageView(page: page)
+                        Spacer()
+                        if page == pages.last {
+                            Button("Let's go!", action:{ self.finishedTutorial = true})
+                                .buttonStyle(.bordered)
+                        } else {
+                            Button("next", action:
+                                incrementPage)
+                        }
                     }
+                    .tag(page.tag)
                 }
-                .tag(page.tag)
             }
+            .animation(.easeInOut, value: pageIndex)
+            .tabViewStyle(.page)
+            .indexViewStyle(.page(backgroundDisplayMode:
+                    .interactive))
+            .onAppear {
+                dotAppearance.currentPageIndicatorTintColor
+                    = .black
+                dotAppearance.pageIndicatorTintColor = .gray
+            }
+            .onAppear(perform: {
+                UserDefaults.standard.tutorialShown = true
+            })
         }
-        .animation(.easeInOut, value: pageIndex)
-        .tabViewStyle(.page)
-        .indexViewStyle(.page(backgroundDisplayMode:
-                .interactive))
-        .onAppear {
-            dotAppearance.currentPageIndicatorTintColor
-                = .black
-            dotAppearance.pageIndicatorTintColor = .gray
-        }
-        .onAppear(perform: {
-            UserDefaults.standard.tutorialShown = true
-        })
+        
     }
     
     func incrementPage() {
