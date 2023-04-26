6c77e3d5916852ed15a44ddb1833caeb570880e8
Jane <tioj@uchicago.edu>
2023-04-26 00:10:18 -0500

    asd

--- GestureApp/PageModel.swift
+++ GestureApp/PageModel.swift
@@ -18,7 +18,7 @@ struct Page: Identifiable, Equatable {
     
     static var samplePages: [Page] = [
     Page(name: "Welcome to Gesture Signaller. This app will allow you to transform gestures into words.", description: "", imageUrl: "first", tag: 0),
-    Page(name: "To perform gestures, please press the button indicated above when running while performing the gesture.", description: "", imageUrl: "sec", tag: 1),
+    Page(name: "To perform gestures, please press the record button when running while performing the gesture.", description: "", imageUrl: "sec", tag: 1),
     Page(name: "After performing gesture, corresponding sound will be made.", description: "", imageUrl: "third", tag: 2)
     ]
 }
