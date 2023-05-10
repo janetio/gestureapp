4c152b94ef4ad8d32d169a962d0ed5e11dc71cbf
MassGallo <gallom@uchicago.edu>
2023-05-09 21:17:55 -0500

    Changed some stuff in record

--- additionalProjects/record/record/ViewController.swift
+++ additionalProjects/record/record/ViewController.swift
@@ -14,7 +14,13 @@ import CoreML
 class ViewController: UIViewController {
     
     var classifier = try? testmodel(configuration: MLModelConfiguration())
-
+    
+    let stateInLength = 400
+    let predictionWindow = 100
+    
+    //Definition in viewDidLoad()
+    var stateInMultiArray : MLMultiArray?
+    
     var attitudeX: [Double] = []
     var attitudeY: [Double] = []
     var attitudeZ: [Double] = []
@@ -41,6 +47,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         startSensorUpdates()
+        stateInMultiArray = try? MLMultiArray(shape: [stateInLength as NSNumber], dataType: MLMultiArrayDataType.double)
     }
     
     @IBOutlet weak var console: UILabel!
@@ -97,55 +104,88 @@ class ViewController: UIViewController {
     
     func getMotionData(deviceMotion:CMDeviceMotion) {
         attitudeX.append(deviceMotion.attitude.pitch)
-                   attitudeY.append(deviceMotion.attitude.roll)
-                   attitudeZ.append(deviceMotion.attitude.yaw)
-                   gyroX.append(deviceMotion.rotationRate.x)
-                   gyroY.append(deviceMotion.rotationRate.y)
-                   gyroZ.append(deviceMotion.rotationRate.z)
-                   gravityX.append(deviceMotion.gravity.x)
-                   gravityY.append(deviceMotion.gravity.y)
-                   gravityZ.append(deviceMotion.gravity.z)
-                   accX.append(deviceMotion.userAcceleration.x)
-                   accY.append(deviceMotion.userAcceleration.y)
-                   accZ.append(deviceMotion.userAcceleration.z)
-            
-                
-            
-        }
+        attitudeY.append(deviceMotion.attitude.roll)
+        attitudeZ.append(deviceMotion.attitude.yaw)
+        gyroX.append(deviceMotion.rotationRate.x)
+        gyroY.append(deviceMotion.rotationRate.y)
+        gyroZ.append(deviceMotion.rotationRate.z)
+        gravityX.append(deviceMotion.gravity.x)
+        gravityY.append(deviceMotion.gravity.y)
+        gravityZ.append(deviceMotion.gravity.z)
+        accX.append(deviceMotion.userAcceleration.x)
+        accY.append(deviceMotion.userAcceleration.y)
+        accZ.append(deviceMotion.userAcceleration.z)
+    }
+    
+    
     
     func predict() -> String? {
         
         if(classifier == nil){
-            console.text! += "Classifier is nil"
+            console.text! += " Classifier is nil "
         }
-            
-        var accXMultiArray = try? MLMultiArray(accX)
-        var accYMultiArray = try? MLMultiArray(accY)
-        var accZMultiArray = try? MLMultiArray(accZ)
         
-        var attitudeXMultiArray = try? MLMultiArray(attitudeX)
-        var attitudeYMultiArray = try? MLMultiArray(attitudeY)
-        var attitudeZMultiArray = try? MLMultiArray(attitudeZ)
+        let length = accX.count
+        
+        if(length < predictionWindow){
+            // Too short of a gesture
+            return "Try doing the gesture for longer"
+        }
+        
+        //The math here could be better i think, but it works ('shares' the trimming from the front and back of the data
+        var numOfLoops = length / predictionWindow
+        var start = (length % predictionWindow) / 2
+        
+        var prediction : String?
+        
+        var accXMultiArray : MLMultiArray?
+        var accYMultiArray : MLMultiArray?
+        var accZMultiArray : MLMultiArray?
+        
+        var attitudeXMultiArray : MLMultiArray?
+        var attitudeYMultiArray : MLMultiArray?
+        var attitudeZMultiArray : MLMultiArray?
 
-        var gravityXMultiArray = try? MLMultiArray(gravityX)
-        var gravityYMultiArray = try? MLMultiArray(gravityY)
-        var gravityZMultiArray = try? MLMultiArray(gravityZ)
+        var gravityXMultiArray : MLMultiArray?
+        var gravityYMultiArray : MLMultiArray?
+        var gravityZMultiArray : MLMultiArray?
         
-        var gyroXMultiArray = try? MLMultiArray(gyroX)
-        var gyroYMultiArray = try? MLMultiArray(gyroY)
-        var gyroZMultiArray = try? MLMultiArray(gyroZ)
+        var gyroXMultiArray : MLMultiArray?
+        var gyroYMultiArray : MLMultiArray?
+        var gyroZMultiArray : MLMultiArray?
         
-        var stateInLength = 400
-        
-        var stateInMultiArray = try? MLMultiArray(shape: [stateInLength as NSNumber], dataType: MLMultiArrayDataType.double)
-        
-        var modelPrediction = try? classifier?.prediction(accX: accXMultiArray!, accY: accYMultiArray!, attitudeX: attitudeXMultiArray!, attitudeY: attitudeYMultiArray!, attitudeZ: attitudeZMultiArray!, gravityX: gravityXMultiArray!, gravityY: gravityYMultiArray!, gravityZ: gravityZMultiArray!, gyroX: gyroXMultiArray!, gyroY: gyroYMultiArray!, gyroZ: gyroZMultiArray!, stateIn: stateInMultiArray!)
-        
-        if(modelPrediction == nil){
-            console.text! += " | modelPrediction is nil"
+        for i in 1...numOfLoops{
+            
+            accXMultiArray = try? MLMultiArray(accX[start ... start + (i * predictionWindow) - 1])
+            accYMultiArray = try? MLMultiArray(accY[start ... start + (i * predictionWindow) - 1])
+            accZMultiArray = try? MLMultiArray(accZ[start ... start + (i * predictionWindow) - 1])
+            
+            attitudeXMultiArray = try? MLMultiArray(attitudeX[start ... start + (i * predictionWindow) - 1])
+            attitudeYMultiArray = try? MLMultiArray(attitudeY[start ... start + (i * predictionWindow) - 1])
+            attitudeZMultiArray = try? MLMultiArray(attitudeZ[start ... start + (i * predictionWindow) - 1])
+
+            gravityXMultiArray = try? MLMultiArray(gravityX[start ... start + (i * predictionWindow) - 1])
+            gravityYMultiArray = try? MLMultiArray(gravityY[start ... start + (i * predictionWindow) - 1])
+            gravityZMultiArray = try? MLMultiArray(gravityZ[start ... start + (i * predictionWindow) - 1])
+            
+            gyroXMultiArray = try? MLMultiArray(gyroX[start ... start + (i * predictionWindow) - 1])
+            gyroYMultiArray = try? MLMultiArray(gyroY[start ... start + (i * predictionWindow) - 1])
+            gyroZMultiArray = try? MLMultiArray(gyroZ[start ... start + (i * predictionWindow) - 1])
+            
+            var modelPrediction = try? classifier?.prediction(accX: accXMultiArray!, accY: accYMultiArray!, attitudeX: attitudeXMultiArray!, attitudeY: attitudeYMultiArray!, attitudeZ: attitudeZMultiArray!, gravityX: gravityXMultiArray!, gravityY: gravityYMultiArray!, gravityZ: gravityZMultiArray!, gyroX: gyroXMultiArray!, gyroY: gyroYMultiArray!, gyroZ: gyroZMultiArray!, stateIn: stateInMultiArray!)
+            
+            if(modelPrediction == nil){
+                console.text! += " modelPrediction is nil "
+                return "FAILED: modelPrediction is nil"
+            }
+            
+            stateInMultiArray = modelPrediction?.stateOut
+            prediction = modelPrediction?.label
+            
+            start += 100
         }
         
-        return modelPrediction?.label
+        return prediction
     }
     
     func saveSensorDataToCsv(fileName:String) {
