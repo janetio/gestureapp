3345e97d8cd2b38474732c5aa16e478e5deb7f8e
MassGallo <gallom@uchicago.edu>
2023-05-23 19:39:04 -0500

    made a new record_tuemay23evening
    This record contains jane's entire new other data. It is 97%training accurate, 98% validation accurate, and according to the rest of jane's new data (wave and updown) it is 93% testing accurate.

---
+++ additionalProjects/record_tuemay23evening/record_tuemay23evening/EditGestures.swift
@@ -0,0 +1,50 @@
+//
+//  EditGestures.swift
+//  record
+//
+//  Created by Jane Tio on 5/15/23.
+//
+
+import SwiftUI
+
+struct Success: View {
+    var body: some View {
+        Text("SUCCESS")
+    }
+}
+
+struct EditGestures: View {
+    
+    @State var upDown = ""
+    @State var wave = ""
+    @State var changed = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                NavigationLink(destination: Success(), isActive: $changed) { EmptyView() }
+                Form {
+                    Section {
+                        TextField("UpDown", text:
+                        $upDown)
+                        TextField("Wave", text:
+                        $wave)
+                    }
+                }
+                Button("Save Changes", action: change)
+                
+            }
+        }
+    }
+    
+    func change() {
+        if upDown.count > 0 {
+            Gestures.upDown = upDown
+        }
+        if wave.count > 0 {
+            Gestures.wave = wave
+        }
+        self.changed = true
+    }
+}
+
---
+++ additionalProjects/record_tuemay23evening/record_tuemay23evening/PageView.swift
@@ -0,0 +1,52 @@
+//
+//  PageView.swift
+//  GestureApp
+//
+//  Created by Jane Tio on 4/21/23.
+//
+
+import SwiftUI
+import AVKit
+import AVFoundation
+
+struct PageView: View {
+    var page: Page
+    @State var wave = AVPlayer(url:  Bundle.main.url(forResource: "wave", withExtension: "mov")!)
+    @State var updown = AVPlayer(url:  Bundle.main.url(forResource: "updown", withExtension: "mov")!)
+    var body: some View {
+        VStack(spacing: 20) {
+            if page.tag <= 2 || page.tag == 5 {
+                Image("\(page.imageUrl)")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .cornerRadius(30)
+                    .cornerRadius(10)
+                    .padding()
+            }
+            if page.tag == 3 {
+                VideoPlayer(player: wave)
+            }
+            if page.tag == 4 {
+                VideoPlayer(player: updown)
+            }
+            Spacer()
+            Spacer()
+            Text(page.name)
+                .font(.title)
+            Spacer()
+            
+            Text(page.description)
+                .font(.subheadline)
+                .frame(width: 300)
+            Spacer()
+        }
+    }
+    
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(page: Page.samplePage)
+    }
+}
